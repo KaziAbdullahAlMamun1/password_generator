84786bcf638d4e3d0dfcb9a199412d3f41ca8403
KaziAbdullahAlMamun1 <kamamun093@gmail.com>
2025-03-10 23:37:22 +0600

    Add files via upload
    final update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import pyperclip
 from random import choice,randint,shuffle
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -36,17 +37,47 @@ def added():
     website = website_entry.get()
     email =email_entry.get()
     password =password_entry.get()
+    new_data = {
+        website:{
+            "email": email,
+            "password": password,
+        }
+    }
     if (website=="" or email=="" or password == ""):
         messagebox.showinfo(message="please don't leave any field empty",title= "website")
-        return
-    is_ok = messagebox.askokcancel(message=f"There are the details entered: \nEmail: {email}\nPassword: {password}\nIs it ok to save?")
-    if is_ok:
-        file = open("data.txt","a")
+       
+    else:
+        try:
+            with open("data.json","r") as data_file:
+                
+                content = json.load(data_file)
+                
+        except FileNotFoundError:
+            with open("data.json","w") as data_file:
+                json.dump(new_data,data_file,indent=4)
+        else:
+            content.update(new_data)
+            with open("data.json","w") as data_file:
+                json.dump(content,data_file,indent=4)
+        finally:
+            website_entry.delete(0,END)
+            password_entry.delete(0,END)
 
-        file.write(f"{website} | {email} | {password}\n")
-        website_entry.delete(0,END)
-        password_entry.delete(0,END)
-        file.close()
+
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json","r") as data_file:
+            content = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="error",message="No data file found")
+    else:
+    
+        
+        try:
+            messagebox.showinfo(title=website,message=f"email: {content[website]["email"]}\npassword: {content[website]["password"]}")
+        except KeyError:
+            messagebox.showerror(title="error",message="No details for the website exits")
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -59,23 +90,24 @@ canvas.create_image(100,100,image=lock_photo)
 canvas.grid(row=0,column=1)
 
 website_label = Label(text= "Website",font=('calibre',10, 'bold'))
-website_entry = Entry(window,width=35)
+website_entry = Entry(width=35)
 website_entry.focus()
 
 email_label = Label(text="Email/Username",font=('calibre',10, 'bold'))
-email_entry = Entry(window,width=35)
+email_entry = Entry(width=30)
 email_entry.insert(0,"hellomello@gmail.com")
 
 password_label = Label(text="Password",font=('calibre',10, 'bold'))
-password_entry = Entry(window,width= 21)
+password_entry = Entry(width= 27)
 
-
+search_btn = Button(text="Search",font=('calibre',10, 'bold'),command=search)
 pass_btn = Button(text="Generate Password",font=('calibre',10, 'bold'),command=new_password)
 
 add_btn = Button(text="Add",width=36,font=('calibre',10, 'bold'),command=added)
 
 website_label.grid(row=1,column=0)
 website_entry.grid(row=1,column=1,columnspan=2)
+search_btn.grid(row=1,column=2)
 email_label.grid(row=2,column=0)
 email_entry.grid(row=2,column=1,columnspan=2)
 password_label.grid(row=3,column=0)
